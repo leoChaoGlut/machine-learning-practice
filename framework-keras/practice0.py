ed85907cdf9d97b476fa96203efb970b0c97eb68
liaochao.lc <liaochao.lc@alibaba-inc.com>
2018-05-03 12:06:36 +0800

    add keras practice

---
+++ framework-keras/practice0.py
@@ -0,0 +1,19 @@
+# 对于具有2个类的单输入模型（二进制分类）：
+from keras import Sequential
+from keras.layers import Dense
+
+model = Sequential()
+model.add(Dense(32, activation='relu', input_dim=100))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+# 生成虚拟数据
+import numpy as np
+
+data = np.random.random((1000, 100))
+labels = np.random.randint(2, size=(1000, 1))
+
+# 训练模型，以 32 个样本为一个 batch 进行迭代
+model.fit(data, labels, epochs=10, batch_size=32)
